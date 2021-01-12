6f7322c6b425bc9a12053ac63f656cc1c59d89ce
mikaelMS <schmidt.m.work@googlemail.com>
2021-01-12 01:10:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# linear_problem_solver
---
+++ lp.py
@@ -0,0 +1,13 @@
+from scipy.optimize import linprog
+
+# neg F values 
+c = [-4, -3]
+A = [[1, 3],
+    [-1, 2]]
+b = [9, 2]
+
+res = linprog(c, A_ub=A, b_ub=b, bounds= (0, None))
+
+print("Resultate gesamt:\n", res)
+print("optimaler Wert:", -res.fun)
+print("optimaler Punkt:", res.x)
