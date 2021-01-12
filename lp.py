b8487848bb3ead53e83455f025d4bee3ce4357eb
mikaelMS <schmidt.m.work@googlemail.com>
2021-01-12 01:39:46 +0100

    test git 2.3.0

--- lp.py
+++ lp.py
@@ -8,6 +8,7 @@ b = [9, 2]
 
 res = linprog(c, A_ub = A, b_ub = b, bounds = (0, None))
 
+# printing
 print("Resultate gesamt:\n", res)
 print("optimaler Wert:", -res.fun)
 print("optimaler Punkt:", res.x)
