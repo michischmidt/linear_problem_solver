48e3ae6f911100c5792ff0f9760399e9fecb9286
mikaelMS <schmidt.m.work@googlemail.com>
2021-01-12 01:14:41 +0100

    minor update

--- lp.py
+++ lp.py
@@ -6,7 +6,7 @@ A = [[1, 3],
     [-1, 2]]
 b = [9, 2]
 
-res = linprog(c, A_ub=A, b_ub=b, bounds= (0, None))
+res = linprog(c, A_ub = A, b_ub = b, bounds = (0, None))
 
 print("Resultate gesamt:\n", res)
 print("optimaler Wert:", -res.fun)
